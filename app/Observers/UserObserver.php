fb46ed06e8e4165cae503d565a657b0818265420
cairumeng <cairumeng@gmail.com>
2020-09-19 17:25:40 +0200

    add avatar & description to users

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -8,6 +8,6 @@ class UserObserver
 {
     public function creating(User $user)
     {
-        $user->avatar= ''
+        $user->avatar = env('APP_URL') . '/images/avatars/default_avatar.png';
     }
 }
