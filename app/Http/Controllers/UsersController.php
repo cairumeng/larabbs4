cd064ecc78abb6b6c7269c2fbfe719272fbd5e14
cairumeng <cairumeng@gmail.com>
2020-09-24 20:30:52 +0200

    user policy

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,10 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => 'show']);
+    }
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -17,12 +21,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, UserRequest $request)
     {
-
+        $this->authorize('update', $user);
         $user->update([
             'name' => $request->name,
             'email' => $request->email,
@@ -36,6 +41,7 @@ class UsersController extends Controller
 
     public function uploadAvatar(User $user, Request $request, ImageUploadHandler $uploader)
     {
+        $this->authorize('update', $user);
         $result = $uploader->save($request->file, 'avatars', $user->id, 416);
 
         if ($result) {
